9f450d18ff51713afd5377df3ce8e252677e8803
dnksebastian <sebastian.dulniawka@gmail.com>
2023-12-31 21:04:16 +0100

    chakra ui

--- chakraui-practice/src/main.jsx
+++ chakraui-practice/src/main.jsx
@@ -1,14 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ChakraProvider>
+// extending the theme
+const colors = {
+  brand: {
+    900: '#024fc9',
+    800: '#146af5',
+    700: '#2977f2',
+    600: '#337df2',
+    500: '#4287f5'
+  },
+}
+const fonts = {
+  body: 'Tahoma',
+  heading: 'Courier New'
+}
+
+const theme = extendTheme({ colors, fonts })
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ChakraProvider theme={theme}>
+    <React.StrictMode>
       <App />
-    </ChakraProvider>
-  </React.StrictMode>
-);
+    </React.StrictMode>
+  </ChakraProvider>
+)
