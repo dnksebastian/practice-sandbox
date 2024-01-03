1e820279c0e6feac798198ed4dad469c9fde9521
dnksebastian <sebastian.dulniawka@gmail.com>
2024-01-03 19:26:51 +0100

    tailwind practice

--- tailwind-foodapp/tailwind.config.js
+++ tailwind-foodapp/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        mammoth: '8rem'
+      },
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883'
+        }
+      },
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
   },
   plugins: [],
 }
