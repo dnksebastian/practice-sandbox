adb6f1e775ee8451f71b2e3b03fc80d453b727b4
dnksebastian <sebastian.dulniawka@gmail.com>
2024-01-26 20:40:31 +0100

    react router

--- react-router6/src/App.jsx
+++ react-router6/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Link, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Link, Route, Routes, Navigate } from 'react-router-dom'
 
 // pages
 import Home from './pages/Home'
@@ -6,8 +6,11 @@ import About from './pages/About'
 import Products from './pages/Products'
 import ProductDetails from './pages/ProductDetails'
 
+import { useState } from 'react'
+
 
 function App() {
+  const [cartIsEmpty] = useState(false);
 
   return (
     <div className="App">
@@ -18,18 +21,23 @@ function App() {
         <Link to="/about">About</Link>
         <Link to="/products">Products</Link>
       </nav>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/products/:id">
-          <ProductDetails />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about/*" element={<About />} />
+        <Route path="/products/:id/*" element={<ProductDetails />}/>
+        <Route path="/products" element={<Products />} />
+        <Route path="/test" element={(
+          <div>
+            <h2>Test Page</h2>
+            <p>Hello</p>
+          </div>
+        )} />
+        <Route path='/redirect' element={<Navigate to='/about' />} />
+        <Route
+        path='/checkout'
+        element={cartIsEmpty ? <Navigate to='/products' /> : <p>checkout</p>}
+        />
+      </Routes>
     </BrowserRouter>
   </div>
   )
