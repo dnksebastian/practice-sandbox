b1750fe29ca248cf1adb2c5adaf26a451caab0cd
dnksebastian <sebastian.dulniawka@gmail.com>
2024-03-24 20:31:22 +0100

    typescript intro

--- typescript-intro/sandbox.ts
+++ typescript-intro/sandbox.ts
@@ -1,9 +1,65 @@
-const character = 'mario';
+let character = 'mario';
+let age = 30;
+let isBlackBelt = false;
 
-console.log(character);
+// TS infers types of variables above...
 
-const inputs = document.querySelectorAll('input');
+// character = 20;
+character = 'luigi'
 
-inputs.forEach(input => {
-  console.log(input);
-});
+// age = 'yoshi'
+age = 30
+
+// isBlackBelt = 'yes'
+isBlackBelt = true;
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI
+};
+
+// console.log(circ('hello'))
+console.log(circ(7.5));
+
+
+// arrays
+let names = ['luigi', 'mario', 'yoshi']
+
+// names = 'hello'
+
+names.push('toad');
+// names.push(3)
+// names[0] = 3
+
+let numbers = [10, 20, 30, 40]
+numbers.push(25)
+// numbers.push('lorem')
+// numbers[1] = 'ipsum'
+
+let mixed = ['ken', 4, 'chun-li', 8, 9, true]; // ts now infers the array can have 3 types
+mixed.push('ryu')
+mixed.push(10);
+mixed[0] = 3;
+
+
+// objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+}
+
+ninja.age = 40
+ninja.name = 'ryu'
+
+// ninja.age = '30'
+// ninja.skills = ['fighting', 'sneaking'] // cant add new properties once we define an object
+// ninja = [] // cant change the type of object
+
+// object has to have the same structure of initial object
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+  // skills: []
+}
+
