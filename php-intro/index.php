842a9f10a89c1ab8b66d808b0a70b38d23c755fe
dnksebastian <sebastian.dulniawka@gmail.com>
2024-04-06 20:20:36 +0200

    php intro

---
+++ php-intro/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    // echo 'hello world';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First PHP file</title>
+</head>
+<body>
+    <h1><?php echo 'hello world'; ?></h1> 
+</body>
+</html>
