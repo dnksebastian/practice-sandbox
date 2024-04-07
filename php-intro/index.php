8f88d4e96ff7da71a5a1891e37d32d8f901e0546
dnksebastian <sebastian.dulniawka@gmail.com>
2024-04-07 15:56:06 +0200

    php intro

--- php-intro/index.php
+++ php-intro/index.php
@@ -1,6 +1,28 @@
 <?php
-
+    define('NAME', 'Yoshi'); //declaring constans which can't be overwritten
     // echo 'hello world';
+    $name = 'Yoshi';
+    $age = 30;
+    // $name = 'Mario';
+
+    // STRINGS:
+    // $stringOne = 'my email is ';
+    // $stringTwo = 'lorem@ipsum.co.uk';
+
+    // echo $stringOne . $stringTwo; //concatenation
+
+    // $name = 'mario';
+    // echo 'Hey, my name is ' . $name;
+
+    // echo "Hey, my name is $name"; // double quotes allow to use variables directly inside (interpolation)
+
+    // echo "The ninja screamed \"whaa\""; //escaping characters with backslash
+    // echo $name[0]; //accessing index in string
+
+    // echo strlen($name); //checking length
+    // echo strtoupper($name);
+    // echo strtolower($name);
+    // echo str_replace('m', 'w', $name); //replacing m's with w's in given string
 
 ?>
 
@@ -12,6 +34,8 @@
     <title>First PHP file</title>
 </head>
 <body>
-    <h1><?php echo 'hello world'; ?></h1> 
+    <h1><?php echo 'hello world'; ?></h1>
+    <div><?php echo NAME; ?></div>
+    <div><?php echo $age; ?></div>
 </body>
 </html>
