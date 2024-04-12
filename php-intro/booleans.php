9cc88185d8a549251b324ac027ba8e7b40cf6ca1
dnksebastian <sebastian.dulniawka@gmail.com>
2024-04-12 18:59:21 +0200

    php intro

---
+++ php-intro/booleans.php
@@ -0,0 +1,49 @@
+<?php 
+
+// comparisons booleans (true / false)
+
+// echo true; // "1"
+// echo false; // ""
+
+// numbers
+
+// echo 5 < 10; // returns 1 (true)
+// echo 5 > 10;
+// echo 5 == 10; // loose comparison
+// echo 10 == 10;
+// echo 5 != 10; // negation
+// echo 5 <= 5; // less or equal
+// echo 5 >= 5; // more or equal
+
+// strings
+
+// echo 'shaun' < 'yoshi'; // compares alphabetically
+// echo 'shaun' > 'yoshi';
+// echo 'shaun' > 'Shaun'; // uppercase letters are seen as less than lowercase
+// echo 'mario' == 'mario'; //true
+// echo 'mario' == 'Mario'; //false
+
+
+// loose vs strict equal comparison
+// echo 5 == '5'; // doesnt take into consideration the type of data = true
+// echo 5 === '5' // strict comparison, takes into account the type = false
+
+// echo 5 == 5;
+// echo 5 === 5;
+
+// echo true == '1'; // true
+// echo false == ''; //true
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Booleans</title>
+</head>
+<body>
+    
+</body>
+</html>
