e1b62a5fa137dd837c468c4d47eedab38e3e97b9
dnksebastian <sebastian.dulniawka@gmail.com>
2024-04-16 19:14:39 +0200

    php intro

--- php-intro/index.php
+++ php-intro/index.php
@@ -5,12 +5,8 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ninja Pizza</title>
-</head>
-<body>
+    <?php include('templates/header.php'); ?>
+
+    <?php include('templates/footer.php'); ?>
     
-</body>
 </html>
