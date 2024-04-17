426ea139e846b166d9957452b0efabc96ce5a472
dnksebastian <sebastian.dulniawka@gmail.com>
2024-04-17 18:23:44 +0200

    php intro

---
+++ php-intro/add.php
@@ -0,0 +1,38 @@
+<?php 
+
+    // if (isset($_GET['submit'])) {
+    //     echo htmlspecialchars($_GET['email']);
+    //     echo htmlspecialchars($_GET['title']);
+    //     echo htmlspecialchars($_GET['ingredients']);
+    // }
+
+    if (isset($_POST['submit'])) {
+        echo htmlspecialchars($_POST['email']);
+        echo htmlspecialchars($_POST['title']);
+        echo htmlspecialchars($_POST['ingredients']);
+    }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <?php include('templates/header.php'); ?>
+
+    <section class="container grey-text">
+        <h4 class="center">Add a Pizza</h4>
+        <form action="add.php" method="POST" class="white">
+            <label for="">Your Email:</label>
+            <input type="text" name="email">
+            <label for="">Pizza Title:</label>
+            <input type="text" name="title">
+            <label for="">Ingredients (comma searated):</label>
+            <input type="text" name="ingredients">
+            <div class="center">
+                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+            </div>
+        </form>
+    </section>
+
+    <?php include('templates/footer.php'); ?>
+    
+</html>
