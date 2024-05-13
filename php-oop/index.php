27c828e39b6777e6a2cf41b9c1157d48f3a36791
dnksebastian <sebastian.dulniawka@gmail.com>
2024-05-13 19:41:34 +0200

    php oop

---
+++ php-oop/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+class User {
+
+    // properties and methods
+
+}
+
+$userOne = new User(); // creating new instance of class
+$userTwo = new User();
+
+get_class($userOne); // tells what class the object is based on
+echo 'the class is ' . get_class($userTwo);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP</title>
+</head>
+<body>
+    
+</body>
+</html>
