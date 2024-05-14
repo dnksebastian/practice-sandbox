23a073c65020e59dfcf3fa4c6676459fc0140b9f
dnksebastian <sebastian.dulniawka@gmail.com>
2024-05-14 13:11:04 +0200

    php oop

--- php-oop/index.php
+++ php-oop/index.php
@@ -1,8 +1,14 @@
 <?php 
 
 class User {
+    // properties
+    public $username = 'ryu';
+    public $email = 'ryu@thenetninja.co.uk';
 
-    // properties and methods
+    // methods
+    public function addFriend() {
+        return "$this->username added a new friend";
+    }
 
 }
 
@@ -11,6 +17,24 @@ $userTwo = new User();
 
 get_class($userOne); // tells what class the object is based on
 echo 'the class is ' . get_class($userTwo);
+
+
+echo $userOne->username . '<br />';
+echo $userOne->email . '<br />';
+echo $userOne->addFriend() . '<br />';
+
+// $userTwo->username = 'yoshi';
+// $userTwo->email = 'yoshi@thenetninja.co.uk';
+
+echo $userTwo->username . '<br />';
+echo $userTwo->email . '<br />';
+echo $userTwo->addFriend() . '<br />';
+
+// utility functions for classes
+// print_r(get_class_vars('User'));
+// print_r(get_class_methods('User'));
+
+
 
 ?>
 
