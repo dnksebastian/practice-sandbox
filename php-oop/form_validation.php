1b9aec2202253004ebfcb65f961730a929da844a
dnksebastian <sebastian.dulniawka@gmail.com>
2024-05-23 19:31:25 +0200

    php oop

--- php-oop/form_validation.php
+++ php-oop/form_validation.php
@@ -1,10 +1,16 @@
 <?php
 
-if(isset($_POST['submit'])){
-    // validate entries
-    echo 'form submitted';
-  }
+require('user_validator.php');
 
+  $errors = [];
+
+  if(isset($_POST['submit'])){
+    // validate entries
+    $validation = new UserValidator($_POST);
+    $errors = $validation->validateForm();
+
+    // if errors is empty --> save data to db
+  }
 
 ?>
 
@@ -22,13 +28,17 @@ if(isset($_POST['submit'])){
     <h2>Create a new user</h2>
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
 
-      <label>Username: </label>
-      <input type="text" name="username">
-
-      <label>Email: </label>
-      <input type="text" name="email">
-
-      <input type="submit" value="submit" name="submit">
+      <label>username: </label>
+      <input type="text" name="username" value="<?php echo !empty($_POST['username']) ? htmlspecialchars($_POST['username']) : ' ' ?>">
+      <div class="error">
+        <?php echo $errors['username'] ?? '' ?>
+      </div>
+      <label>email: </label>
+      <input type="text" name="email" value="<?php echo !empty($_POST['email']) ? htmlspecialchars($_POST['email']) : ' ' ?>">
+      <div class="error">
+        <?php echo $errors['email'] ?? '' ?>
+      </div>
+      <input type="submit" value="submit" name="submit" >
 
     </form>
   </div>
