70e9b7d5948404bcf20bce358cb5984ab1d5e63d
dnksebastian <sebastian.dulniawka@gmail.com>
2024-08-14 18:48:13 +0200

    htmx page

--- htmx-page/routes/index.js
+++ htmx-page/routes/index.js
@@ -13,4 +13,17 @@ router.get('/articles/:id', (req, res) => {
   res.render('article', { title: article.name, article });
 });
 
+router.post('/articles', (req, res) => {
+  const { name, body } = req.body;
+
+  const article = {
+    id: Math.floor(Math.random() * 1000000),
+    name,
+    body
+  };
+  articles.push(article);
+
+  res.render('partials/list', { articles });
+});
+
 export default router;
